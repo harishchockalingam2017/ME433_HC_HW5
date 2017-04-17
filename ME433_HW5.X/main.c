789f93c08f0b30a9bc0bf6b4d637a0cc0b7b4048
harishchockalingam <harishchockalingam2017@u.northwestern.edu>
2017-04-17 00:19:20 -0500

    start i2c

---
+++ ME433_HW5.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Harish
+ *
+ * Created on April 17, 2017, 12:18 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
